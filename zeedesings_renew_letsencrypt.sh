9dc015a22641464a7e1e46863e15381b6515a8c9
cactixxx <eduplooy@gmail.com>
2021-05-17 20:40:21 +0200

    Update zeedesings_renew_letsencrypt.sh

--- zeedesings_renew_letsencrypt.sh
+++ zeedesings_renew_letsencrypt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "pop" > /tmp/ppp
 /usr/bin/certbot renew -q
 /usr/bin/cat /etc/letsencrypt/live/zeedesigns.co.za/fullchain.pem /etc/letsencrypt/live/zeedesigns.co.za/privkey.pem > /etc/ssl/zeedesigns.co.za/zeedesigns.co.za.pem
