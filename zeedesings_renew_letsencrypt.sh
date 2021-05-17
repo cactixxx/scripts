cf77ad2637ee10260d861ccb7d600e9acf3d0d1f
cactixxx <eduplooy@gmail.com>
2021-05-17 20:35:16 +0200

    Create zeedesings_renew_letsencrypt.sh

---
+++ zeedesings_renew_letsencrypt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/bin/certbot renew -q
+/usr/bin/cat /etc/letsencrypt/live/zeedesigns.co.za/fullchain.pem /etc/letsencrypt/live/zeedesigns.co.za/privkey.pem > /etc/ssl/zeedesigns.co.za/zeedesigns.co.za.pem
